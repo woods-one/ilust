56def045c1078581eda68146b01458ca873e62e7
woods-one <83874472+woods-one@users.noreply.github.com>
2022-01-31 14:19:23 +0900

    Create compta.c

---
+++ cfile/compta.c
@@ -0,0 +1,17 @@
+      #i        nclu            
+    de  <s    td    io
+    .h    >int      ma
+    in                ()
+  {w    hi    le      (1
+  ){      pr            in
+  tf    ("konn          po
+ta                      kunn
+ka                        waii
+")                          ;p
+ri                            tf
+"H                            el
+loWo                          rl
+  d"                        }ret
+  urn0;}//anaumezonedesugomenn
+
+//コンポタくんです
